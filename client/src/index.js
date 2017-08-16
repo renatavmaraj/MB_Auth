768bb206c6a9a512a9380b80c8f00a49f3c33cbc
Renata Maraj <renatavmaraj@gmail.com>
2017-08-16 13:34:49 -0500

    Add token persistence

--- client/src/index.js
+++ client/src/index.js
@@ -13,11 +13,19 @@ import Feature from './components/feature';
 import Landing from './components/landing';
 import RequireAuth from './components/auth/require_auth';
 import reducers from './reducers';
+import { AUTH_USER } from './actions/types';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+const token = localStorage.getItem('token');
+//If we have a token, consder the user to be signed in
+if(token) {
+  //update application state
+  store.dispatch({ type: AUTH_USER });
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
       <IndexRoute component={Landing}/>
